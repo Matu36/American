1455422f080414e569caf5bc256a7c0649d13e0e
Matu36 <matipineda85@live.com.ar>
2024-06-11 19:31:46 -0300

    estilos

--- client/src/components/Admin/NavBarAdmin.jsx
+++ client/src/components/Admin/NavBarAdmin.jsx
@@ -17,11 +17,8 @@ export default function NavBarAdmin() {
       <div className="sidebarAdmin__header">
         <button onClick={handleButtonClick} className="topadmin__button">
           <img src={logo} alt="chef" className="sidebarAdmin__image" />
+          <h1 className="navbarAdmin__title">Panel de Administrador</h1>
         </button>
-      </div>
-
-      <div className="navbarAdmin__left">
-        <h1 className="navbarAdmin__title">Panel de Administrador</h1>
       </div>
       <div className="navbarAdmin__right">
         <div className="navbarAdmin__menu"></div>
