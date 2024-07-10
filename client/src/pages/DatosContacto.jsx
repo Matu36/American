3520e05cf8f79a1e2337e1c15a8289d8200eab82
Matu36 <matipineda85@live.com.ar>
2024-07-10 20:52:44 -0300

    contacto

--- client/src/pages/DatosContacto.jsx
+++ client/src/pages/DatosContacto.jsx
@@ -6,53 +6,61 @@ export default function DatosContacto() {
     <div className="datosContacto">
       <div className="grupoContacto">
         <h3>Ventas</h3>
-        <a
-          href="https://instagram.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a target="_blank" rel="noopener noreferrer">
           <FaPhoneSquareAlt size={40} color="#FF7F50" />
           4748-5900
         </a>
-        <a href="https://wa.me" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://wa.me/1159249700"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaWhatsapp size={40} color="#25D366" />
           11 5924-9700
         </a>
       </div>
       <div className="grupoContacto">
         <h3>Post Venta</h3>
-        <a
-          href="https://instagram.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="tel:47485900" target="_blank" rel="noopener noreferrer">
           <FaPhoneSquareAlt size={40} color="#FF7F50" />
           4748-5900
         </a>
-        <a href="https://wa.me" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://wa.me/1139284834"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaWhatsapp size={40} color="#25D366" />
           11 3928-4834
         </a>
-        <a href="https://wa.me" target="_blank" rel="noopener noreferrer">
+        <a
+          href="mailto:servicios@americanvial.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaEnvelope size={40} color="#808080" />
           servicios@americanvial.com
         </a>
       </div>
       <div className="grupoContacto">
         <h3>Repuestos</h3>
-        <a
-          href="https://instagram.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a target="_blank" rel="noopener noreferrer">
           <FaPhoneSquareAlt size={40} color="#FF7F50" />
           4748-5900
         </a>
-        <a href="https://wa.me" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://wa.me/1151469600"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaWhatsapp size={40} color="#25D366" />
           11 5146-9600
         </a>
-        <a href="https://wa.me" target="_blank" rel="noopener noreferrer">
+        <a
+          href="mailto:repuestos@americanvial.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaEnvelope size={40} color="#808080" />
           repuestos@americanvial.com
         </a>
