de0497d74737f1d4566642087f09bcac738086a1
Matu36 <matipineda85@live.com.ar>
2024-08-13 14:29:10 -0300

    interceptor que no anda

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -117,71 +117,107 @@ router.post("/usuarios/login", login);
 router.post("/usuarios/registro", registro);
 router.post("/usuarios/rol", check.auth, verificarRol);
 router.put("/usuarios", check.auth, putUser);
-router.get("/usuarios/lastFive", getLastLoggedInUsers);
-router.get("/usuarios/chart", getUsuariosChart);
-router.get("/usuarios/detail/:idUsuario", obtenerDetalleUsuario);
+router.get("/usuarios/lastFive", check.auth, getLastLoggedInUsers);
+router.get("/usuarios/chart", check.auth, getUsuariosChart);
+router.get("/usuarios/detail/:idUsuario", check.auth, obtenerDetalleUsuario);
 router.get("/usuarios/all", check.auth, getAllUsers);
-router.get("/usuarios/vendedores", getUsuariosConRolFalse);
+router.get("/usuarios/vendedores", check.auth, getUsuariosConRolFalse);
 router.put("/usuarios/recoverpass", resetPassword);
 router.get("/productos/getAll", getProductos);
-router.get("/productos/getParaCotizar", getProductosParaCotizar);
+router.get("/productos/getParaCotizar", check.auth, getProductosParaCotizar);
 router.get("/productos/getCategorias", getProductosPorCategoria);
 router.get("/productos/getMarcas", getProductosPorMarca);
 router.get("/productos/getDivisiones", getProductosPorDivision);
 router.get("/productos/:id", getProductoById);
 router.get("/productos/familia/:familia", getProductosPorFamilia);
-router.get("/contactoProducto/detalle/:id", getContactoProductoById);
-router.get("/contacto/detalle/:id", getContactoById);
+router.get(
+  "/contactoProducto/detalle/:id",
+  check.auth,
+  getContactoProductoById
+);
+router.get("/contacto/detalle/:id", check.auth, getContactoById);
 router.post("/productos/create", check.auth, createProducto);
 router.put("/productos/edit", check.auth, putProductos);
 router.delete("/productos/delete", check.auth, deleteProducto);
 router.post("/cotizaciones/create", check.auth, createCotizacion);
-router.get("/cotizaciones/get/:idUsuario", getCotizaciones);
-router.get("/cotizaciones/getCotizacionesSum/:idUsuario", getCotizacionesSum);
-router.get("/cotizaciones/getDetalle/:idCotizacion", getCotizacionDetalle);
-router.get("/cotizaciones/getVentas/:idUsuario", getCotizacionesEstadoDos);
-router.get("/countCotizaciones/get", getCantidadCotizacionesPorUsuario);
-router.get("/Cotizaciones/ranking", getranking);
+router.get("/cotizaciones/get/:idUsuario", check.auth, getCotizaciones);
+router.get(
+  "/cotizaciones/getCotizacionesSum/:idUsuario",
+  check.auth,
+  getCotizacionesSum
+);
+router.get(
+  "/cotizaciones/getDetalle/:idCotizacion",
+  check.auth,
+  getCotizacionDetalle
+);
+router.get(
+  "/cotizaciones/getVentas/:idUsuario",
+  check.auth,
+  getCotizacionesEstadoDos
+);
+router.get(
+  "/countCotizaciones/get",
+  check.auth,
+  getCantidadCotizacionesPorUsuario
+);
+router.get("/Cotizaciones/ranking", check.auth, getranking);
 router.put("/cotizaciones/edit", check.auth, putCotizaciones);
 router.post("/clientes/create", check.auth, createCliente);
 router.get("/clientes/get/:idUsuario", getClientesPorIdDeUsuario);
 router.get("/clientes/getDetalle/:idCliente", getClientePorId);
 router.get("/clientes/getParaCotizar/:idUsuario", getClientesParaCotizar);
 router.put("/clientes/edit", check.auth, updateCliente);
-router.get("/contacto/get", getContactos);
-router.get("/contacto/getNoLeidosCount", countActiveContactos);
-router.put("/contacto/put", updateContactoState);
-router.put("/contacto/derivado", updateContactoUsuario);
+router.get("/contacto/get", check.auth, getContactos);
+router.get("/contacto/getNoLeidosCount", check.auth, countActiveContactos);
+router.put("/contacto/put", check.auth, updateContactoState);
+router.put("/contacto/derivado", check.auth, updateContactoUsuario);
 router.post("/contacto/create", createContacto);
-router.get("/contactoProducto/get", getContactoProducto);
+router.get("/contactoProducto/get", check.auth, getContactoProducto);
 router.post("/contactoProducto/create", createContactoProducto);
-router.put("/contactoProducto/derivado", updateContactoProductoUsuario);
+router.put(
+  "/contactoProducto/derivado",
+  check.auth,
+  updateContactoProductoUsuario
+);
 router.put("/cotizaciones/state", check.auth, updateCotizacionEstado);
-router.get("/cotizaciones/suma", sumarPreciosFinales);
-router.get("/cotizaciones/ultimas/:idUsuario", getUltimasCotizaciones);
-router.get("/cotizaciones/sumaVentas", sumarPreciosFinalesPorMonedaYEstado);
-router.get("/usuarios/mensajes", getAllUsersMensajes);
+router.get("/cotizaciones/suma", check.auth, sumarPreciosFinales);
+router.get(
+  "/cotizaciones/ultimas/:idUsuario",
+  check.auth,
+  getUltimasCotizaciones
+);
+router.get(
+  "/cotizaciones/sumaVentas",
+  check.auth,
+  sumarPreciosFinalesPorMonedaYEstado
+);
+router.get("/usuarios/mensajes", check.auth, getAllUsersMensajes);
 router.post("/mensajes/create", check.auth, createMessage);
 router.put("/mensajes/put", check.auth, updateMessageState);
-router.get("/mensajes/get", getMessagesByUserAndDestination);
-router.get("/mensajes/count/:idUsuario", countMessagesByDestination);
-router.get("/mensajes/enviados/:idUsuario", getMensajesByUsuario);
-router.get("/mensajes/detail/:idMensaje", obtenerMensajePorId);
-router.get("/mensajes/recibidos/:idUsuario", getMensajesByDestino);
+router.get("/mensajes/get", check.auth, getMessagesByUserAndDestination);
+router.get(
+  "/mensajes/count/:idUsuario",
+  check.auth,
+  countMessagesByDestination
+);
+router.get("/mensajes/enviados/:idUsuario", check.auth, getMensajesByUsuario);
+router.get("/mensajes/detail/:idMensaje", check.auth, obtenerMensajePorId);
+router.get("/mensajes/recibidos/:idUsuario", check.auth, getMensajesByDestino);
 router.get("/solicitaciones/get", getAllSolicitantes);
 router.post("/solicitaciones/create", createSolicitante);
-router.get("/garantias/garantiasById/:id", getGarantiaById);
-router.get("/garantias/getAll", getAllGarantias);
+router.get("/garantias/garantiasById/:id", check.auth, getGarantiaById);
+router.get("/garantias/getAll", check.auth, getAllGarantias);
 router.post("/garantias/create", createGarantia);
-router.put("/garantias/put", updateGarantiaState);
-router.get("/garantias/getNoLeidosCount", countActiveGarantias);
-router.get("/cotizaciones/getVentasById/:id", getVentaById);
-router.post("/historial/vendedor/", getHistorialDetallePorUsuario);
-router.post("/historial/modelo/", getCotizacionesPorModelo);
-router.post("/cotizaciones/fecha/", filtrarCotizacionesPorFecha);
+router.put("/garantias/put", check.auth, updateGarantiaState);
+router.get("/garantias/getNoLeidosCount", check.auth, countActiveGarantias);
+router.get("/cotizaciones/getVentasById/:id", check.auth, getVentaById);
+router.post("/historial/vendedor/", check.auth, getHistorialDetallePorUsuario);
+router.post("/historial/modelo/", check.auth, getCotizacionesPorModelo);
+router.post("/cotizaciones/fecha/", check.auth, filtrarCotizacionesPorFecha);
 router.post("/repuestos/create", createRepuesto);
-router.get("/repuestos/get", getRepuestos);
-router.get("/repuestos/detalle/:id", getRepuestoById);
-router.put("/repuestos/derivado", updateRepuestoUsuario);
-router.put("/garantias/derivado", updateGarantiaUsuario);
+router.get("/repuestos/get", check.auth, getRepuestos);
+router.get("/repuestos/detalle/:id", check.auth, getRepuestoById);
+router.put("/repuestos/derivado", check.auth, updateRepuestoUsuario);
+router.put("/garantias/derivado", check.auth, updateGarantiaUsuario);
 module.exports = router;
--- client/src/hooks/useAxiosInterceptor.jsx
+++ client/src/hooks/useAxiosInterceptor.jsx
@@ -7,17 +7,28 @@ const useAxiosInterceptor = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
+    // Agregar interceptor para respuesta exitosa
     const interceptor = axios.interceptors.response.use(
-      (response) => response,
+      (response) => {
+        console.log("Respuesta exitosa:", response); // Log de la respuesta exitosa
+        return response;
+      },
       (error) => {
+        console.log("Error de respuesta:", error); // Log del error de respuesta
+
         if (error.response && error.response.status === 401) {
+          console.log(
+            "Token expirado o no autorizado. Redirigiendo al inicio."
+          );
           localStorage.removeItem("token");
-          navigate("/"); // Redirigir al home si la sesión expiró
+          navigate("/");
         }
+
         return Promise.reject(error);
       }
     );
 
+    // Eliminar interceptor cuando el componente se desmonte
     return () => {
       axios.interceptors.response.eject(interceptor);
     };
