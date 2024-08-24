fd993b9cb7cb09abc9a508fc64059b2acb62caad
Matu36 <matipineda85@live.com.ar>
2024-08-24 10:33:22 -0300

    responsivo

--- client/src/components/ErroPage.jsx
+++ client/src/components/ErroPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import error from "../assets/img/error404.png";
+import errorResponsivo from "../assets/img/error404responsivo.png";
 
 export default function ErroPage() {
   return (
     <div className="fullscreen-image">
-      <img src={error} alt="Error Image" />
+      <img className="responsivo" src={errorResponsivo} alt="ErrorR Image" />
+      <img className="ordenador" src={error} alt="Error Image" />
     </div>
   );
 }
