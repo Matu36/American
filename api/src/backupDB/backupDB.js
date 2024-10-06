cc0b126322e4e95bab61bc48bf6b509d0c2c5a8c
Matu36 <matipineda85@live.com.ar>
2024-10-05 23:36:07 -0300

    backup e interceptor

--- api/src/backupDB/backupDB.js
+++ api/src/backupDB/backupDB.js
@@ -2,20 +2,30 @@ const { exec } = require("child_process");
 const cron = require("node-cron");
 const moment = require("moment");
 const path = require("path");
+const url = require("url");
+
+// Función para obtener información de la URL de la base de datos
+const getDatabaseConfig = () => {
+  const dbUrl = process.env.DATABASE_URL;
+  const parsedUrl = url.parse(dbUrl);
+
+  const PGHOST = parsedUrl.hostname;
+  const PGPORT = parsedUrl.port;
+  const [PGUSER, PGPASSWORD] = parsedUrl.auth.split(":");
+  const PGDATABASE = parsedUrl.pathname.split("/")[1];
+
+  return { PGHOST, PGPORT, PGUSER, PGPASSWORD, PGDATABASE };
+};
 
 // Función para realizar el respaldo de la base de datos
 const backupDatabase = () => {
+  const { PGHOST, PGPORT, PGUSER, PGPASSWORD, PGDATABASE } =
+    getDatabaseConfig();
   const date = moment().format("YYYY-MM-DD_HH-mm-ss"); // Formato para la fecha
   const backupFile = path.join("/mnt/data", `backup_${date}.sql`);
 
   // Comando para realizar el backup
-  const command = `PGPASSWORD=${process.env.PGPASSWORD} pg_dump --host=${
-    process.env.PGHOST
-  } --port=${process.env.PGPORT} --username=${
-    process.env.PGUSER
-  } --no-password --verbose --file=${backupFile} --format=custom ${
-    process.env.POSTGRES_DB || process.env.PGDATABASE
-  }`;
+  const command = `PGPASSWORD=${PGPASSWORD} pg_dump --host=${PGHOST} --port=${PGPORT} --username=${PGUSER} --no-password --verbose --file=${backupFile} --format=custom ${PGDATABASE}`;
 
   exec(command, (error, stdout, stderr) => {
     if (error) {
@@ -36,4 +46,5 @@ cron.schedule("0 12 * * *", () => {
   backupDatabase();
 });
 
+// Exportar la función de respaldo para uso externo
 module.exports = backupDatabase;
--- client/src/hooks/useAxiosInterceptor.jsx
+++ client/src/hooks/useAxiosInterceptor.jsx
@@ -1,41 +1,50 @@
 // hooks/useAxiosInterceptor.js
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const useAxiosInterceptor = () => {
   const navigate = useNavigate();
 
-  // Interceptor para las solicitudes
-  axios.interceptors.request.use(
-    (config) => {
-      const token = localStorage.getItem("token");
-      if (token) {
-        config.headers.Authorization = token;
+  useEffect(() => {
+    // Interceptor para las solicitudes
+    const requestInterceptor = axios.interceptors.request.use(
+      (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+          config.headers.Authorization = token;
+        }
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
       }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-  );
+    );
 
-  // Interceptor para las respuestas
-  axios.interceptors.response.use(
-    (response) => response,
-    (error) => {
-      const { response } = error;
+    // Interceptor para las respuestas
+    const responseInterceptor = axios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        const { response } = error;
 
-      if (response) {
-        if (response.status === 401) {
-          if (response.data && response.data.message === "token inválido") {
-            localStorage.removeItem("token");
-            navigate("/");
+        if (response) {
+          if (response.status === 401) {
+            if (response.data && response.data.message === "token inválido") {
+              localStorage.removeItem("token");
+              navigate("/");
+              return; // Evita que se propague el error después de redirigir
+            }
           }
         }
+        return Promise.reject(error);
       }
-      return Promise.reject(error);
-    }
-  );
+    );
+
+    return () => {
+      axios.interceptors.request.eject(requestInterceptor);
+      axios.interceptors.response.eject(responseInterceptor);
+    };
+  }, [navigate]);
 };
 
 export default useAxiosInterceptor;
